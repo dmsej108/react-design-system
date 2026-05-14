03b7e0071c911f4fe2db45ea41b1e2580de249d2
zzou <smilejjou@gmail.com>
2026-05-14 17:39:06 +0900

    S prefix

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,44 @@
-export { Button } from './components/Button';
-export type { ButtonProps, ButtonVariant, ButtonSize } from './components/Button';
+export { Button as SButton } from './components/Button';
+export type { ButtonProps as SButtonProps, ButtonVariant as SButtonVariant, ButtonSize as SButtonSize } from './components/Button';
 
-export { Input } from './components/Input';
-export type { InputProps, InputSize } from './components/Input';
+export { Input as SInput } from './components/Input';
+export type { InputProps as SInputProps, InputSize as SInputSize } from './components/Input';
 
-export { Card } from './components/Card';
-export type { CardProps, CardVariant, CardSize } from './components/Card';
+export { Card as SCard } from './components/Card';
+export type { CardProps as SCardProps, CardVariant as SCardVariant, CardSize as SCardSize } from './components/Card';
 
-export { Badge } from './components/Badge';
-export type { BadgeProps, BadgeVariant, BadgeSize } from './components/Badge';
+export { Badge as SBadge } from './components/Badge';
+export type { BadgeProps as SBadgeProps, BadgeVariant as SBadgeVariant, BadgeSize as SBadgeSize } from './components/Badge';
 
-export { Typography } from './components/Typography';
-export type { TypographyProps, TypographyVariant, TypographyColor } from './components/Typography';
+export { Typography as STypography } from './components/Typography';
+export type { TypographyProps as STypographyProps, TypographyVariant as STypographyVariant, TypographyColor as STypographyColor } from './components/Typography';
 
-export { FileInput } from './components/FileInput';
-export type { FileInputProps } from './components/FileInput';
+export { FileInput as SFileInput } from './components/FileInput';
+export type { FileInputProps as SFileInputProps } from './components/FileInput';
 
-export { Modal } from './components/Modal';
-export type { ModalProps, ModalSize, ModalFooterAlign } from './components/Modal';
+export { Modal as SModal } from './components/Modal';
+export type { ModalProps as SModalProps, ModalSize as SModalSize, ModalFooterAlign as SModalFooterAlign } from './components/Modal';
 
-export { ToastProvider, useToast } from './components/Toast';
-export type { ToastProviderProps, ToastOptions, ToastItem, ToastVariant, ToastPosition } from './components/Toast';
+export { ToastProvider as SToastProvider, useToast } from './components/Toast';
+export type { ToastProviderProps as SToastProviderProps, ToastOptions, ToastItem, ToastVariant, ToastPosition } from './components/Toast';
 
-export { Select } from './components/Select';
-export type { SelectProps, SelectSize, SelectOption } from './components/Select';
+export { Select as SSelect } from './components/Select';
+export type { SelectProps as SSelectProps, SelectSize as SSelectSize, SelectOption } from './components/Select';
 
-export { Icon } from './components/Icon';
-export type { IconProps, IconName, IconSize } from './components/Icon';
+export { Icon as SIcon } from './components/Icon';
+export type { IconProps as SIconProps, IconName, IconSize } from './components/Icon';
 
-export { DatePicker } from './components/DatePicker';
-export type { DatePickerProps } from './components/DatePicker';
+export { DatePicker as SDatePicker } from './components/DatePicker';
+export type { DatePickerProps as SDatePickerProps } from './components/DatePicker';
 
-export { DateRangePicker } from './components/DateRangePicker';
-export type { DateRangePickerProps } from './components/DateRangePicker';
+export { DateRangePicker as SDateRangePicker } from './components/DateRangePicker';
+export type { DateRangePickerProps as SDateRangePickerProps } from './components/DateRangePicker';
 
-export { Pagination } from './components/Pagination';
-export type { PaginationProps } from './components/Pagination';
+export { Pagination as SPagination } from './components/Pagination';
+export type { PaginationProps as SPaginationProps } from './components/Pagination';
 
-export { SearchItem } from './components/SearchItem';
-export type { SearchItemProps } from './components/SearchItem';
+export { SearchItem as SSearchItem } from './components/SearchItem';
+export type { SearchItemProps as SSearchItemProps } from './components/SearchItem';
 
-export { BaseButton } from './components/BaseButton';
-export type { BaseButtonProps } from './components/BaseButton';
+export { BaseButton as SBaseButton } from './components/BaseButton';
+export type { BaseButtonProps as SBaseButtonProps } from './components/BaseButton';
